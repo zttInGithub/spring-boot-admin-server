a0da797e8999b0c6dd17f075cefb04a9b4d5824a
hrt <269941971@qq.com>
2020-05-08 09:15:42 +0800

    5.8.1

--- src/main/java/com/ztt/test/SpringBootAdminServerApplication.java
+++ src/main/java/com/ztt/test/SpringBootAdminServerApplication.java
@@ -3,6 +3,7 @@ package com.ztt.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
